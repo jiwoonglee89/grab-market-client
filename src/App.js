d3f8fd48726a5766bced349ba49b38e63b92465e
jiwoong_lee <leewo1001@gmail.com>
2023-02-20 18:47:03 +0900

    ㅎㅇ

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MainPageCompoenet from "./main/index.js";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
-  return <MainPageCompoenet />;
+  return (
+    <div>
+      <switch>
+        <Route exact={true} path={"/"}>
+          <MainPageCompoenet />
+        </Route>
+        <Route exact={true} path={"/products/:id"}>
+          <ProductPage />
+        </Route>
+        <Route exact={true} path={"/upload"}>
+          <UploadPage />
+        </Route>
+      </switch>
+    </div>
+  );
 }
 
 export default App;
